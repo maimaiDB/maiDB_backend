cff68b56112514a7e61ec530538e12c3c8cffec1
Loop1397 <shwlsdn0906@gmail.com>
2026-04-18 04:00:22 +0900

    chore: 유저를 발견하지 못한 모든 경우에 대응하기 위해 USER_NOT_FOUNDED의 메세지 변경

--- src/common/exception/error.code.ts
+++ src/common/exception/error.code.ts
@@ -15,5 +15,5 @@ export type ErrorCode = ErrorCodeVo;
 
 // 아래에 에러 코드 값 객체를 생성하여 export
 export const INVALID_ID_FORMAT = new ErrorCodeVo(400, 'INVALID_ID_FORMAT', 'ID 형식이 올바르지 않습니다.');
-export const USER_NOT_FOUNDED = new ErrorCodeVo(404, 'USER_NOT_FOUNDED', '해당 ID의 유저가 발견되지 않았습니다.');
+export const USER_NOT_FOUNDED = new ErrorCodeVo(404, 'USER_NOT_FOUNDED', '해당 유저가 발견되지 않았습니다.');
 export const EMAIL_ALREADY_EXISTS = new ErrorCodeVo(409, 'EMAIL_ALREADY_EXISTS', '이미 사용 중인 이메일입니다.');
