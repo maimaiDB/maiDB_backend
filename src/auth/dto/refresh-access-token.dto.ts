24fdd70dd95bc577526d4a9658b56e2227f6f766
Loop1397 <shwlsdn0906@gmail.com>
2026-04-17 19:38:46 +0900

    feature: dto 내용 작성

--- src/auth/dto/refresh-access-token.dto.ts
+++ src/auth/dto/refresh-access-token.dto.ts
@@ -1,1 +1,7 @@
-export class RefreshAccessTokenDto { }
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class RefreshAccessTokenDto {
+    @IsString({ message: '리프레시 토큰은 문자열이어야 합니다.' })
+    @IsNotEmpty({ message: '리프레시 토큰은 필수 입력 항목입니다.' })
+    refreshToken: string;
+}
