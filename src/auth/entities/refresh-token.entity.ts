9f51844c847439292c3c7432123f29c27152b9ae
Loop1397 <shwlsdn0906@gmail.com>
2026-04-17 22:51:35 +0900

    feature: refresh-token entity 작성
    user와 1:N 관계 설정

---
+++ src/auth/entities/refresh-token.entity.ts
@@ -0,0 +1,33 @@
+import { User } from "src/user/entities/user.entity";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * CHECKLIST
+ * [ ] TODO: USER와의 관계 설정
+ */
+
+@Entity('refresh_tokens') // 테이블 이름을 'users'로 설정
+export class RefreshToken extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    token: string;
+
+    @Column({ nullable: false })
+    expiresAt: Date;
+
+    @Column({ nullable: true })
+    revokedAt: Date;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @ManyToOne(
+        () => User,
+        user => user.refreshTokens,
+        { nullable: false, onDelete: 'CASCADE' }
+    )
+    @JoinColumn({ name: 'userId' })
+    user: User;
+}
