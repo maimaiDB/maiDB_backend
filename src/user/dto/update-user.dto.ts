33ef04f8ffadb7aec45e6f84b7f116fbe280c028
Loop <shwlsdn0906@gmail.com>
2026-05-04 17:14:57 +0900

    feature: 보안 이슈를 방지하기 위해 role 필드는 업데이트 DTO에서 제외

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -26,7 +26,9 @@ export class UpdateUserDto extends PartialType(SignUpDto) {
   @IsBoolean({ message: '공개 여부는 boolean 값이어야 합니다.' })
   isPublic?: boolean;
 
-  @IsOptional()
-  @IsString({ message: '관리 권한 여부는 문자열이어야 합니다.' })
-  role?: string;
+  // NOTE: 혹시 모를 보안 이슈 방지를 위해 일단 role 필드는 업데이트에서 제외
+  // 추후에 관리자 권한이 필요한 별도의 DTO로 분리할까?
+  //   @IsOptional()
+  //   @IsString({ message: '관리 권한 여부는 문자열이어야 합니다.' })
+  //   role?: string;
 }
