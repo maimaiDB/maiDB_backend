8f7c82025f27d5b46cbd3ede4e67ff881332e8d3
Loop1397 <shwlsdn0906@gmail.com>
2026-05-12 22:53:24 +0900

    feature: 이미 존재하는 노래를 만들려고 했을 때 출력하는 오류 작성

--- src/common/exception/service.exception.ts
+++ src/common/exception/service.exception.ts
@@ -1,4 +1,4 @@
-import { EMAIL_ALREADY_EXISTS, ErrorCode, INVALID_CREDENTIALS, INVALID_ID_FORMAT, INVALID_JWT_FORMAT, INVALID_JWT_TOKEN, REFRESH_TOKEN_NOT_FOUND, TOKEN_EXPIRED, USER_NOT_FOUNDED, ACCESS_DENIED } from "./error.code";
+import { EMAIL_ALREADY_EXISTS, ErrorCode, INVALID_CREDENTIALS, INVALID_ID_FORMAT, INVALID_JWT_FORMAT, INVALID_JWT_TOKEN, REFRESH_TOKEN_NOT_FOUND, TOKEN_EXPIRED, USER_NOT_FOUNDED, ACCESS_DENIED, SONG_TITLE_ALREADY_EXISTS } from "./error.code";
 
 // error 클래스를 상속받은 ServiceException 클래스 정의
 // serviceException 클래스는 NestJS의 ExceptionFilter에서 감지하기 위한 커스텀 예외 클래스
@@ -48,4 +48,8 @@ export const RefreshTokenNotFoundedException = (message?: string): ServiceExcept
 
 export const EmailAlreadyExistsException = (message?: string): ServiceException => {
     return new ServiceException(EMAIL_ALREADY_EXISTS, message);
+}
+
+export const SongTitleAlreadyExistsException = (message?: string): ServiceException => {
+    return new ServiceException(SONG_TITLE_ALREADY_EXISTS, message);
 }
