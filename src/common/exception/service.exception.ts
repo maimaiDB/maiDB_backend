1bfc1573b33ff928d1319f236f207a43f0a06162
Loop1397 <shwlsdn0906@gmail.com>
2026-06-04 16:15:46 +0900

    feature: pattern에서 사용될 오류 코드 작성

--- src/common/exception/service.exception.ts
+++ src/common/exception/service.exception.ts
@@ -1,4 +1,4 @@
-import { EMAIL_ALREADY_EXISTS, ErrorCode, INVALID_CREDENTIALS, INVALID_ID_FORMAT, INVALID_JWT_FORMAT, INVALID_JWT_TOKEN, REFRESH_TOKEN_NOT_FOUND, TOKEN_EXPIRED, USER_NOT_FOUNDED, ACCESS_DENIED, SONG_TITLE_ALREADY_EXISTS, SONG_NOT_FOUNDED, TAG_NAME_NOT_FOUNDED, TAG_NAME_ALREADY_EXISTS, PROFILE_NOT_FOUNDED } from "./error.code";
+import { EMAIL_ALREADY_EXISTS, ErrorCode, INVALID_CREDENTIALS, INVALID_ID_FORMAT, INVALID_JWT_FORMAT, INVALID_JWT_TOKEN, REFRESH_TOKEN_NOT_FOUND, TOKEN_EXPIRED, USER_NOT_FOUNDED, ACCESS_DENIED, SONG_TITLE_ALREADY_EXISTS, SONG_NOT_FOUNDED, TAG_NAME_NOT_FOUNDED, TAG_NAME_ALREADY_EXISTS, PROFILE_NOT_FOUNDED, PATTERN_NOT_FOUND, PATTERN_ALREADY_EXISTS } from "./error.code";
 
 // error 클래스를 상속받은 ServiceException 클래스 정의
 // serviceException 클래스는 NestJS의 ExceptionFilter에서 감지하기 위한 커스텀 예외 클래스
@@ -58,6 +58,10 @@ export const RefreshTokenNotFoundedException = (message?: string): ServiceExcept
     return new ServiceException(REFRESH_TOKEN_NOT_FOUND, message);
 }
 
+export const PatternNotFoundedException = (message?: string): ServiceException => {
+    return new ServiceException(PATTERN_NOT_FOUND, message);
+}
+
 export const EmailAlreadyExistsException = (message?: string): ServiceException => {
     return new ServiceException(EMAIL_ALREADY_EXISTS, message);
 }
@@ -68,4 +72,8 @@ export const SongTitleAlreadyExistsException = (message?: string): ServiceExcept
 
 export const TagNameAlreadyExistsException = (message?: string): ServiceException => {
     return new ServiceException(TAG_NAME_ALREADY_EXISTS, message);
+}
+
+export const PatternAlreadyExistsException = (message?: string): ServiceException => {
+    return new ServiceException(PATTERN_ALREADY_EXISTS, message);
 }
