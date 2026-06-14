3c7ddd1a81761314317b9f9b564a7779929c0f24
RYU <me@ryu.sh>
2026-06-14 15:22:25 +0900

    fix: Song에 pattern이 profiles로 걸려있던 문제 수정

--- src/pattern/entities/pattern.entity.ts
+++ src/pattern/entities/pattern.entity.ts
@@ -1,41 +1,48 @@
-import { Difficulty } from "src/pattern/enums/difficulty.enum";
-import { Song } from "src/song/entities/song.entity";
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Difficulty } from 'src/pattern/enums/difficulty.enum';
+import { Song } from 'src/song/entities/song.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('patterns')
 export class Pattern extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    // 난이도(BASIC, ADVANCED, EXPERT, MASTER, RE:MASTER)
-    @Column({ nullable: false, type: 'enum', enum: Difficulty })
-    difficulty: Difficulty;
+  // 난이도(BASIC, ADVANCED, EXPERT, MASTER, RE:MASTER)
+  @Column({ nullable: false, type: 'enum', enum: Difficulty })
+  difficulty!: Difficulty;
 
-    // 표기 레벨
-    @Column({ nullable: false })
-    level: string;
+  // 표기 레벨
+  @Column({ nullable: false })
+  level!: string;
 
-    // 내부 상수 레벨
-    @Column({ nullable: true })
-    internalLevel: number;
+  // 내부 상수 레벨
+  @Column({ nullable: true })
+  internalLevel?: number;
 
-    // DX 여부
-    @Column({ nullable: false })
-    isDx: boolean;
+  // DX 여부
+  @Column({ nullable: false })
+  isDx!: boolean;
 
-    // 채보 제작자
-    @Column({ nullable: true })
-    chartDesigner: string;
+  // 채보 제작자
+  @Column({ nullable: true })
+  chartDesigner?: string;
 
-    // 채보 추천도
-    @Column({ nullable: true })
-    recommendationScore: string;
+  // 채보 추천도
+  @Column({ nullable: true })
+  recommendationScore?: string;
 
-    // 체감 난이도
-    @Column({ nullable: true })
-    perceivedDifficulty: string;
+  // 체감 난이도
+  @Column({ nullable: true })
+  perceivedDifficulty?: string;
 
-    @ManyToOne(() => Song, song => song.profiles)
-    @JoinColumn({ name: 'songId' })
-    song: Song;
+  @ManyToOne(() => Song, (song) => song.patterns)
+  @JoinColumn({ name: 'songId' })
+  song!: Song;
 }
